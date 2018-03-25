eaf2c78dd3f45c2f53043b1fd925ae9bd11dd792
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2018-03-25 21:53:25 +0530

    Added templete font-end for home

--- public/js/controllers/home.controller.js
+++ public/js/controllers/home.controller.js
@@ -4,5 +4,32 @@ angular
 
 homeController.$inject = ['$scope'];
 function homeController($scope) {
-  $scope.msg = "Welcome from Home page";
+  $scope.msg = "Welcome from E-krishi";
+
+  $scope.teamMember = [
+    {
+      name: 'Shashank S Shetty',
+      image: 'img/default.jpg',
+      info: 'Front End Designed',
+      description: 'I wanted to make this application, which will be done '
+    },
+    {
+      name: 'Spandhana ND',
+      image: 'img/default.jpg',
+      info: 'Documation Head',
+      description: 'I wanted to make this application, which will be done '
+    },
+    {
+      name: 'Shreyas Kanchan',
+      image: 'img/default.jpg',
+      info: 'Assisant Documation & Researcher',
+      description: 'I wanted to make this application, which will be done '
+    },
+    {
+      name: 'Noushand',
+      image: 'img/default.jpg',
+      info: 'Lead Researcher',
+      description: 'I wanted to make this application, which will be done '
+    }
+  ];
 }

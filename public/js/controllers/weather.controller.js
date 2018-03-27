c7cefdaa9d4ace0cf5e5312b2dbe6164d03965fc
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2018-03-28 00:27:58 +0530

    Added some styling for news and weather section

--- public/js/controllers/weather.controller.js
+++ public/js/controllers/weather.controller.js
@@ -1,6 +1,12 @@
 angular
   .module('myApp')
-  .controller('weatherController', weatherController);
+  .controller('weatherController', weatherController)
+  .filter("reverse", function() {
+    return function(input) {
+      input = input.split('-');  // split based in '-'
+      return result = input[2] + '-' + input[1] + '-' + input[0];
+    };
+  });
 
 weatherController.$inject = ['$scope', '$timeout', 'WeatherService'];
 function weatherController($scope, $timeout , WeatherService) {
@@ -12,6 +18,9 @@ function weatherController($scope, $timeout , WeatherService) {
       console.log(response);
       if(response.length !== 0) {
         $scope.zipcode = response.zip_code;
+        $scope.city = response.city;
+        $scope.region_name = response.region_name;
+        $scope.country_name = response.country_name
       } 
     });
 

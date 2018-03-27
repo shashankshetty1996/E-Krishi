8815159a5946d610bf8cbc566d80eddee9f9e866
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2018-03-27 20:27:43 +0530

    Added pre loader for fetching of the news

--- public/js/controllers/news.controller.js
+++ public/js/controllers/news.controller.js
@@ -1,13 +1,14 @@
 angular.module("myApp").controller("newsController", newsController);
 
-newsController.$inject = ["$scope", "NewsService"];
-function newsController($scope, NewsService) {
+newsController.$inject = ["$scope", '$timeout', "NewsService"];
+function newsController($scope, $timeout, NewsService) {
   $scope.msg = "Get Updated with what's actually happening!";
+  $scope.loader = "Collecting Information"
+  $scope.preloader = true;  
 
-  $scope.news = [
-    {id: 1, link: '#!news/1', title: 'Title 1', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dignissimos dolorem deleniti ipsum eos est at quam, esse aliquam, perspiciatis voluptas itaque fuga necessitatibus maxime voluptatibus suscipit exercitationem, quasi autem.'},
-    {id: 2, link: '#!news/2', title: 'Title 2', content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim aperiam maiores excepturi similique, quam a omnis sint doloremque eligendi modi numquam, explicabo alias libero aspernatur temporibus officiis impedit assumenda?'},    
-  ];
+  $timeout(function() {
+    $scope.preloader = false;      
+  }, 3000);
 
   NewsService.GetAll()
     .then(function(response) {
